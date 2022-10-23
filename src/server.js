bb99eadf45fd4c6c42fdcfb6a40457cd0db7ac46
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-10-23 20:38:36 -0300

    corrige bugs

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 require('dotenv/config');
 const app = require('./app');
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
