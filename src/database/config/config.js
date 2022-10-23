54c71f4b97048d637aefae94c4f182d1d97f7782
Carlos Daniel <carlosdanielpereirasantos18@gmail.com>
2022-10-23 15:52:47 -0300

    template sequelize with mysql and express

---
+++ src/database/config/config.js
@@ -0,0 +1,27 @@
+require('dotenv/config');
+
+const development = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: process.env.MYSQL_DIALECT,
+};
+
+const test = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: process.env.MYSQL_DIALECT,
+};
+
+const production = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: process.env.MYSQL_DIALECT,
+};
+
+module.exports = { development, test, production };
